ee8f247083eb8a0c3424901c6680f0933880998a
StringManolo <manuelvarelacaldas@gmail.com>
2025-12-19 13:19:34 +0000

    Rm: Remove typescript. Using a lower level languaje

--- src/cli/ui.ts
+++
@@ -1,9 +0,0 @@
-export const showHelp = (c: any): void => {
-  console.log(`
-  ${c.brightMagenta.bold("Bahamut")}
-
-  ${c.bold.cyan("•")} ${c.bold.yellow("Start")}     ${c.dim.white("start scanner service")}
-  ${c.bold.cyan("•")} ${c.bold.yellow("Stop")}      ${c.dim.white("stop scanner service")}
-
-  `);
-}
